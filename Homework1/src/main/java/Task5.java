6c9f7ab74cc51b01a47656559349f447a8d53a5d
lukazagar96@gmail.com <Killerman1710>
2018-04-09 20:45:22 +0200

    Task 6 and 7

--- Homework1/src/main/java/Task5.java
+++ Homework1/src/main/java/Task5.java
@@ -1,8 +1,10 @@
 public class Task5 {
     public static void main(String arg[])
     {
-        int[] largeArray = {1,2,3,4,4,5,9,1,2};
-        System.out.println(largestNumber(largeArray));
+        int[] array = {1,2,3,4,4,5,9,1,2};
+        System.out.println("Largest number in array is: " + largestNumber(array));
+        array = oddPositions(array);
+        for (int anArray : array) System.out.println(anArray);
     }
 
     private static int largestNumber(int array[])
@@ -13,5 +15,25 @@ public class Task5 {
                 max = anArray;
         return max;
     }
+
+    private static int[] reverseArray(int array[])
+    {
+        int temp[] = new int[array.length];
+        for(int i = 0; i< array.length; i++)
+            temp[i]=array[array.length - 1 - i];
+        return temp;
+    }
+
+    private static int[] oddPositions(int array[])
+    {
+        int temp[] = new int[array.length/2], counter=0;
+        for(int i = 0; i< array.length; i++)
+            if (i % 2 == 1) {
+                temp[counter] = array[i];
+                counter ++;
+            }
+        return temp;
+    }
+
     
 }
